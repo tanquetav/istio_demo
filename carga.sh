beeec518db55c4564c075055a6d0427bf4f55afb
George Tavares <tavares.george@gmail.com>
2018-07-21 20:56:02 -0300

    Novos scripts

--- carga.sh
+++ carga.sh
@@ -1,1 +1,1 @@
-while [ true ]; do curl -H "me: me12"  http://192.168.122.15:31380/info; echo "";  sleep .3; done
+while [ true ]; do curl -H "me: me"  http://192.168.122.15:31380/info; echo "";  sleep .3; done
---
+++ carga2.sh
@@ -0,0 +1,1 @@
+while [ true ]; do curl -H "me: me2"  http://192.168.122.15:31380/info; echo "";  sleep .3; done
